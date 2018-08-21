b0d0f88314c275c61af3b49b2947318de9c1ba1f
anuj97 <anujupadhyayjnp@gmail.com>
2018-08-22 02:03:31 +0530

    Run coala again
    Ran coala through remaining files and code is now consistent

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-import sys
 from cx_Freeze import setup, Executable
 
-build_exe_options = {"packages":["os","RandomWords","random","string"]}
+build_exe_options = {"packages": ["os", "RandomWords", "random", "string"]}
 
-setup( name="Hangman",
-	version = "1.0",
-	description = "A fun and recreational game!",
-	executables = [Executable("hangman.py")])
+setup(name="Hangman",
+      version="1.0",
+      description="A fun and recreational game!",
+      executables=[Executable("hangman.py")])

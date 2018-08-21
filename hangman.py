4cd80fd742acf037ac7f4b5c7bcb5a6de5abe5e0
anuj97 <anujupadhyayjnp@gmail.com>
2018-08-22 00:59:26 +0530

    Add coala
    Added coala and made code consistent with PEP8

--- hangman.py
+++ hangman.py
@@ -1,15 +1,15 @@
 '''
-	Hangman, Fun and recreational game through which one can increase his vocab and thinking abilities. 
+    Hangman, Fun and recreational game through which one can increase his vocab and thinking abilities.
 
-	Requirements - python 3.6, and module RandomWords.
-	
-	how to install Module RandomWords ?
-		pip install RandomWords
+    Requirements - python 3.6, and module RandomWords.
 
-	How to use ?
-	This game will soon be deployed on website, untill then you can clone the repo and directly run the python script on terminal or python GUI.
+    how to install Module RandomWords ?
+        pip install RandomWords
 
-	Developer's:
+    How to use ?
+    This game will soon be deployed on website, untill then you can clone the repo and directly run the python script on terminal or python GUI.
+
+    Developer's:
 		This game is made in two levels one is for beginners and other is for pro players, seperate functions are there for all the activities so it's easy to debug the program. Variables names declared are self_explantory.For any doubt comment on git.
 
 	NOTE: I haven't checked it thouroughly so there might be some unsual crashes.Fixed all the bugs which I'm aware of, while playing this game if you find an error please comment and I'll try to remove that ambiguity. 
